43b915d5bb4dd7365321c2818dff68a9078b4d22
asif <asif69284@gmail.com>
2024-09-04 13:01:08 +0545

    Using filter by filename & testname regex pattern

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -6,14 +6,18 @@
 import { render, screen } from '@testing-library/react'
 import Greet from './Greet'
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText('hello')
-    expect(textElement).toBeInTheDocument()
-})
+describe("Greet", () => {
+    
+    test('Greet renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText('hello')
+        expect(textElement).toBeInTheDocument()
+    })
 
-test('Greet renders with a name', () => {
-    render(<Greet name='Asif' />)
-    const textElement = screen.getByText("hello Asif")
-    expect(textElement).toBeInTheDocument()
+    test('Greet renders a name', () => {
+        render(<Greet name='asif' />)
+        const textElement = screen.getByText("hello asif")
+        expect(textElement).toBeInTheDocument()
+    })
+
 })
